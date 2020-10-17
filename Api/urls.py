73dc0523716dbcaa797072bc9c6c92841269da69
Sunny Thakur <sunny.thakur693@gmail.com>
2020-10-17 12:05:54 +0530

    url

--- Api/urls.py
+++ Api/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 
 urlpatterns=[
             path('admin/', admin.site.urls),
-            path('',include('firstapi.urls')),
+            path('',include('firstapi.url')),
 ]
